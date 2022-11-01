4155524c4c2bd4d3983a2486dda4b502f092b5c0
vlad <vladyslav.sakun@applifting.cz>
2022-11-01 16:48:28 +0100

    Get screen off timeout

--- app/src/main/java/com/example/mvicompose/MVIComposeApp.kt
+++ app/src/main/java/com/example/mvicompose/MVIComposeApp.kt
@@ -1,6 +1,8 @@
 package com.example.mvicompose
 
 import android.app.Application
+import android.provider.Settings
+import android.util.Log
 import com.example.mvicompose.ui.feature.main.BaseAndroidViewModelFactory
 
 class MVIComposeApp : Application() {
@@ -9,9 +11,15 @@ class MVIComposeApp : Application() {
         super.onCreate()
 
         BaseAndroidViewModelFactory.inject(this)
+
+        val mSystemScreenOffTimeOut = Settings.System.getInt(contentResolver, Settings.System.SCREEN_OFF_TIMEOUT)
+
+        Log.d(TAG, "onCreate: mSystemScreenOffTimeOut: $mSystemScreenOffTimeOut")
     }
 
     companion object {
+        private const val TAG = "MVIComposeApp"
+
         init {
             System.loadLibrary("mvicompose")
         }
